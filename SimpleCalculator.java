8299e45612f3a9c9721c205374557191af2cf3f1
Tyler Kain Mason <95332889+KainMason@users.noreply.github.com>
2023-03-23 14:21:48 -0400

    added File

---
+++ SimpleCalculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter the operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Division by zero is not allowed.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operation. Only +, -, *, / are allowed.");
+                return;
+        }
+
+        System.out.printf("The result of %.2f %c %.2f is: %.2f%n", num1, operation, num2, result);
+    }
+}
