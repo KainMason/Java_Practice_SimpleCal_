f406f62970fbe8ee35deb700bb3811d6af3d237e
Tyler Kain Mason <95332889+KainMason@users.noreply.github.com>
2023-03-23 14:25:20 -0400

    closed scanner

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -2,41 +2,41 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
 
-        System.out.print("Enter the first number: ");
-        double num1 = scanner.nextDouble();
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
 
-        System.out.print("Enter the second number: ");
-        double num2 = scanner.nextDouble();
+            System.out.print("Enter the operation (+, -, *, /): ");
+            char operation = scanner.next().charAt(0);
 
-        System.out.print("Enter the operation (+, -, *, /): ");
-        char operation = scanner.next().charAt(0);
+            double result = 0;
 
-        double result = 0;
-
-        switch (operation) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("Division by zero is not allowed.");
+            switch (operation) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("Division by zero is not allowed.");
+                        return;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("Invalid operation. Only +, -, *, / are allowed.");
                     return;
-                }
-                result = num1 / num2;
-                break;
-            default:
-                System.out.println("Invalid operation. Only +, -, *, / are allowed.");
-                return;
-        }
+            }
 
-        System.out.printf("The result of %.2f %c %.2f is: %.2f%n", num1, operation, num2, result);
+            System.out.printf("The result of %.2f %c %.2f is: %.2f%n", num1, operation, num2, result);
+        }
     }
 }
